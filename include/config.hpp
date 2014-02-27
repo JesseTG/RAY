7e9cbcbdb58821e4cccaf2a2d7d74916fa5f21ed
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-02-27 16:56:22 -0500

    Updated config.hpp to include more constants

--- include/config.hpp
+++ include/config.hpp
@@ -4,7 +4,40 @@
 #include <SFML/System.hpp>
 
 namespace ray {
-const sf::Vector2<int> SCREEN_SIZE(640, 480);
+/**
+ * Width of the game resolution, in pixels
+ */
+const int SCREEN_WIDTH = 800;
+
+/**
+ * Height of the game resolution, in pixels
+ */
+const int SCREEN_HEIGHT = 600;
+
+/**
+ * A @c sf::Vector2 that holds the width and height of the screen in pixels.
+ */
+const sf::Vector2i SCREEN_SIZE(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+/**
+ * The desired framerate of the game
+ */
+const int FPS = 60;
+
+/**
+ * The desired period of a frame, in seconds
+ */
+const float SPF = 1.0 / FPS;
+
+/**
+ * The number of iterations the Box2D physics solver will run over velocity
+ */
+const int VELOCITY_ITERATIONS = 8;
+
+/**
+ * The number of iterations the Box2D physics solver will run over position
+ */
+const int POSITION_ITERATIONS = 3;
 }
 
 
