7ba66e1faefac9cbb2e42c6738744058d1ff2c50
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-02-27 17:01:18 -0500

    Moved entity creation steps out of PlayerGunSystem

--- src/systems/PlayerGunSystem.cpp
+++ src/systems/PlayerGunSystem.cpp
@@ -3,9 +3,15 @@
 #include <cmath>
 #include <iostream>
 
+#include "entities.hpp"
 #include "components.hpp"
 
 namespace ray {
+
+using std::hypot;
+using std::cos;
+using std::sin;
+using std::atan2;
 
 const ComponentFilter PlayerGunSystem::FILTER = ComponentFilter()
         .requires<PositionComponent, MouseFollowControlComponent>();
@@ -29,14 +35,16 @@ void PlayerGunSystem::update(const vector<Event>& events) {
             if (e.mouseButton.button == Mouse::Button::Left) {
                 Vector2f worldpos =
                     this->_window->mapPixelToCoords(Vector2i(e.mouseButton.x, e.mouseButton.y));
-                // Where in the world will we create the bullet?
+                // Where in the world will the bullet fly to?
 
-                Entity ent = this->getWorld().createEntity();
-                ent.addComponent(new PositionComponent(worldpos));
-                ent.addComponent(new RenderableComponent);
-                ent.addComponent(new VelocityComponent(1, 1));
-                ent.addComponent(new BulletComponent(5));
-                ent.activate();
+                Vector2f playerpos = this->_player.getComponent<PositionComponent>().position;
+                Vector2f v = worldpos - playerpos;
+                double theta = atan2(v.y, v.x);
+                double vx = 5 * cos(theta);
+                double vy = 5 * sin(theta);
+
+                entities::createBullet(playerpos.x, playerpos.y, vx, vy);
+                entities::createEnemy(worldpos.x, worldpos.y, 8);
             }
         }
     }
