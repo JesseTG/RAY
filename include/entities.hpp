6abd5d40b99db91e9b99f808c43f64e44fcb0cc7
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-02-27 16:56:54 -0500

    More Entity factories now

--- include/entities.hpp
+++ include/entities.hpp
@@ -11,7 +11,12 @@ using anax::World;
 
 using sf::CircleShape;
 using sf::Color;
+using sf::RectangleShape;
 using sf::RenderWindow;
+using sf::Vector2;
+using sf::Vector2i;
+using sf::Vector2f;
+using sf::Vector2u;
 
 extern World*        _world ;
 extern RenderWindow* _window;
@@ -20,16 +25,66 @@ extern RenderWindow* _window;
  * Sets the @c World that will create the @c Entities (so you don't have to
  * keep passing in the @c World as a parameter to each factory function).
  *
- * @param World The @c World that created @c Entities will belong to
+ * @param world The @c World that created @c Entities will belong to
  */
 void setWorld(World&) noexcept;
 
+/**
+ * Sets the @c RenderWindow that will ultimately be rendered to so we don't
+ * have to keep passing it into certain factory functions, as sometimes
+ * information provided by a @c RenderWindow, e.g. mouse position or a @c View
+ * offset, may be necessary.
+ *
+ * @param window The @c RenderWindow used for this game.
+ */
 void setRenderWindow(RenderWindow&) noexcept;
 
-Entity createKeyboardCircle(float, float, float) noexcept;
+/**
+ * Creates and activates circle that can be controlled by the arrow keys. More
+ * specifically, creates an @c Entity with the following components:
+ *
+ * @li RenderableComponent
+ * @li PositionComponent
+ * @li VelocityComponent
+ * @li FourWayControlComponent
+ *
+ * @param r The radius of the @c Entity in pixels
+ * @param x The horizontal position of the @c Entity in the @c World in pixels
+ * @param y The vertical position of the @c Entity in the @c World in pixels
+ *
+ * @return The new @c Entity
+ */
+Entity createKeyboardCircle(const float r, const float x, const float y) noexcept;
 
-Entity createMouseCircle(float) noexcept;
+/**
+ * Creates and activates a crosshair (in the form of a circle) that follows the
+ * mouse. More specifically, creates an @c Entity with the following components:
+ *
+ * @li RenderableComponent
+ * @li PositionComponent
+ * @li MouseFollowComponent
+ *
+ * @param r The radius, in pixels, of the crosshair
+ *
+ * @return The new @c Entity
+ */
+Entity createMouseCircle(const float r) noexcept;
 
+/**
+ * Creates and activates a tractor beam
+ */
+Entity createTractorBeam(
+    const Entity&,
+    const Entity&,
+    const float,
+    const float,
+    const float,
+    const float
+) noexcept;
+
+Entity createEnemy(const float, const float, const float) noexcept;
+
+Entity createBullet(const float, const float, const float, const float) noexcept;
 }
 }
 
--- src/entities.cpp
+++ src/entities.cpp
@@ -15,13 +15,14 @@ void setRenderWindow(RenderWindow& window) noexcept {
     _window = &window;
 }
 
-Entity createKeyboardCircle(float r, float x, float y) noexcept {
+Entity createKeyboardCircle(const float r, const float x, const float y) noexcept {
     Entity e = _world->createEntity();
     CircleShape* circle = new CircleShape(r, 30);
     circle->setFillColor(Color::Red);
     circle->setPosition(x, y);
+    circle->setOrigin(r, r);
 
-    e.addComponent(new RenderableComponent(circle));
+    e.addComponent(new RenderableComponent(circle, 50));
     e.addComponent(new PositionComponent(x, y));
     e.addComponent(new VelocityComponent);
     e.addComponent(new FourWayControlComponent);
@@ -38,11 +39,68 @@ Entity createMouseCircle(float r) noexcept {
     circle->setFillColor(Color::Transparent);
     circle->setOrigin(r, r);
 
-    e.addComponent(new RenderableComponent(circle));
+    e.addComponent(new RenderableComponent(circle, 100));
     e.addComponent(new PositionComponent);
     e.addComponent(new MouseFollowControlComponent);
     _world->activateEntity(e);
 
+    return e;
+}
+
+Entity createTractorBeam(
+    const Entity& face_target,
+    const Entity& follow_target,
+    const float starting_width,
+    const float starting_angle,
+    const float length,
+    const float force) noexcept {
+    Entity e = _world->createEntity();
+
+    RectangleShape* rectangle = new RectangleShape(Vector2f(length, starting_width));
+    rectangle->setFillColor(Color::Blue);
+    rectangle->setOrigin(0.0, starting_width / 2.0);
+
+    e.addComponent(new RenderableComponent(rectangle, 10));
+    e.addComponent(new PositionComponent(256, 256));
+    e.addComponent(new FaceEntityComponent(face_target));
+    e.addComponent(new EntityFollowComponent(follow_target));
+
+    _world->activateEntity(e);
+    return e;
+}
+
+Entity createEnemy(const float x, const float y, const float r) noexcept {
+    Entity e = _world->createEntity();
+    CircleShape* circle = new CircleShape(r, 8);
+    circle->setFillColor(Color::Green);
+    circle->setPosition(x, y);
+    circle->setOrigin(r, r);
+
+    e.addComponent(new RenderableComponent(circle, 500));
+    e.addComponent(new PositionComponent(x, y));
+    e.addComponent(new VelocityComponent);
+    e.addComponent(new AccelerationComponent);
+    e.addComponent(new TractorBeamRepellableComponent);
+
+    _world->activateEntity(e);
+    return e;
+}
+
+Entity createBullet(
+    const float px,
+    const float py,
+    const float vx,
+    const float vy) noexcept {
+    Entity e = _world->createEntity();
+    CircleShape* circle = new CircleShape(8);
+    circle->setOrigin(8, 8);
+
+    e.addComponent(new PositionComponent(px, py));
+    e.addComponent(new RenderableComponent(circle));
+    e.addComponent(new VelocityComponent(vx, vy));
+    e.addComponent(new BulletComponent(2));
+
+    _world->activateEntity(e);
     return e;
 }
 
