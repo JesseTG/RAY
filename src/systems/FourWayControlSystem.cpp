5f62e24cabc68897ade0f60e673ec7f3b67c366a
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-03-02 20:19:43 -0500

    Made FourWayControlSystem more attuned to Box2D

--- src/systems/FourWayControlSystem.cpp
+++ src/systems/FourWayControlSystem.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 
 #include "components.hpp"
+#include "util.hpp"
 
 namespace ray {
 
 const ComponentFilter FourWayControlSystem::FILTER = ComponentFilter()
-        .requires<FourWayControlComponent, PositionComponent, VelocityComponent>()
+        .requires<FourWayControlComponent, PositionComponent, VelocityComponent, PhysicsBodyComponent>()
         .excludes<MouseFollowControlComponent>();
 
 FourWayControlSystem::FourWayControlSystem() : Base(FILTER)
@@ -24,14 +25,15 @@ void FourWayControlSystem::update() {
         // For each Entity available...
         FourWayControlComponent& f = e.getComponent<FourWayControlComponent>();
         VelocityComponent&       v = e.getComponent<VelocityComponent>();
+        PhysicsBodyComponent& pb   = e.getComponent<PhysicsBodyComponent>();
 
         if (Keyboard::isKeyPressed(f.left)) {
             // If the user is trying to go left...
-            v.velocity.x = -5;
+            v.velocity.x = -100;
         }
         else if (Keyboard::isKeyPressed(f.right)) {
             // If the user is trying to go right...
-            v.velocity.x = 5;
+            v.velocity.x = 100;
         }
         else {
             v.velocity.x = 0;
@@ -39,15 +41,17 @@ void FourWayControlSystem::update() {
 
         if (Keyboard::isKeyPressed(f.up)) {
             // If the user is trying to go up...
-            v.velocity.y = -5;
+            v.velocity.y = -100;
         }
         else if (Keyboard::isKeyPressed(f.down)) {
             // If the user is trying to go down...
-            v.velocity.y = 5;
+            v.velocity.y = 100;
         }
         else {
             v.velocity.y = 0;
         }
+
+        pb.body->SetLinearVelocity(sfVecToB2Vec(v.velocity));
     }
 }
 
