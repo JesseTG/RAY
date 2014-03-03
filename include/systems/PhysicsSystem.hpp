728779bcee4c2b7d28ba8426d3d6546c9a4db02d
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-03-02 20:17:03 -0500

    Made the PhysicsSystem useful
    -Now it updates PositionComponents and VelocityComponents
    -I'm beginning to question the usefulness of them, though

--- include/systems/PhysicsSystem.hpp
+++ include/systems/PhysicsSystem.hpp
@@ -5,14 +5,19 @@
 #include <Box2D/Box2D.h>
 
 namespace ray {
+
+using anax::ComponentFilter;
+
 class PhysicsSystem : public anax::System<PhysicsSystem>
 {
     public:
-        PhysicsSystem();
+        PhysicsSystem(b2World&);
         ~PhysicsSystem();
         void update();
     private:
-        b2World _world;
+        b2World* _world;
+
+        static const ComponentFilter FILTER;
 };
 }
 
--- src/systems/PhysicsSystem.cpp
+++ src/systems/PhysicsSystem.cpp
@@ -1,9 +1,16 @@
 #include "PhysicsSystem.hpp"
 
+#include "components.hpp"
 #include "config.hpp"
 
 namespace ray {
-PhysicsSystem::PhysicsSystem() : _world(b2Vec2(0, 0)), Base()
+
+using anax::Entity;
+
+const ComponentFilter PhysicsSystem::FILTER = ComponentFilter()
+        .requires<PhysicsBodyComponent, PositionComponent, VelocityComponent>();
+
+PhysicsSystem::PhysicsSystem(b2World& world) : _world(&world), Base(FILTER)
 {
     //ctor
 }
@@ -13,8 +20,21 @@ PhysicsSystem::~PhysicsSystem()
     //dtor
 }
 
-PhysicsSystem::update() {
-    this->_world.Step(SPF, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+void PhysicsSystem::update() {
+    this->_world->Step(SPF, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+
+    for (Entity& e : this->getEntities()) {
+        PhysicsBodyComponent& phy = e.getComponent<PhysicsBodyComponent>();
+        PositionComponent& posc = e.getComponent<PositionComponent>();
+        VelocityComponent& velc = e.getComponent<VelocityComponent>();
+        const b2Vec2& pos = phy.body->GetPosition();
+        const b2Vec2& vel = phy.body->GetLinearVelocity();
+
+        posc.position.x = pos.x;
+        posc.position.y = pos.y;
+        velc.velocity.x = vel.x;
+        velc.velocity.y = vel.y;
+    }
 }
 
 }
