61dae1ac5cacfb027ac724d8b410c3fb8f4ba850
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-03-09 01:09:23 -0500

    Added a default constructor to PhysicsBodyComponent
    * Makes the class less idiot-proof, though
    * It's to make it easier to create it in Lua-space

--- include/components/PhysicsBodyComponent.hpp
+++ include/components/PhysicsBodyComponent.hpp
@@ -5,6 +5,7 @@
 
 #include <anax/anax.hpp>
 #include <Box2D/Box2D.h>
+#include <LuaContext.hpp>
 
 namespace ray {
 
@@ -28,50 +29,60 @@ using anax::Entity;
  */
 struct PhysicsBodyComponent : public anax::Component<PhysicsBodyComponent>
 {
-    /**
-     * Constructs this PhysicsBodyComponent with a given @c b2Body.
-     *
-     * Upon initialization, will call @c body->SetUserData(&e).
-     * In debug builds, will ensure @c e.isValid() is @c true and that @c body
-     * isn't @c nullptr.
-     *
-     * @param body Pointer to the @c b2Body that the owning @c Entity will
-     * represent.
-     * @param e The @c Entity that will hold @c *this. A pointer to it will be
-     * stored in @c body's user data.
-     */
-    PhysicsBodyComponent(b2Body* body, Entity& e) : body(body), entity(e) {
-#ifdef DEBUG
-        if (!this->body) {
-            // If we got a null b2Body...
-            throw std::invalid_argument("Expected a valid b2Body, got nullptr");
+        /**
+         * Constructs this PhysicsBodyComponent with a given @c b2Body.
+         *
+         * Upon initialization, will call @c body->SetUserData(&e).
+         * In debug builds, will ensure @c e.isValid() is @c true and that @c body
+         * isn't @c nullptr.
+         *
+         * @param body Pointer to the @c b2Body that the owning @c Entity will
+         * represent.
+         * @param e The @c Entity that will hold @c *this. A pointer to it will be
+         * stored in @c body's user data.
+         */
+        PhysicsBodyComponent(b2Body* body, Entity& e) : body(body), entity(e) {
+            this->_set_body(body, e);
         }
 
-        if (!e.isValid()) {
-            // If the given Entity isn't properly attached to a World
-            throw std::logic_error("Cannot use a PhysicsBodyComponent with an invalid Entity");
+        PhysicsBodyComponent() : body(nullptr) {}
+
+        /**
+         * Destructor. Tells the b2World to destroy @c this->body.
+         */
+        ~PhysicsBodyComponent() {
+            this->body->GetWorld()->DestroyBody(this->body);
         }
+
+        /**
+         * Pointer to a @c b2Body that was created by the game's @c b2World. Not a
+         * smart pointer because Box2D objects are managed in a special way that a
+         * smart pointer would fuck up.
+         */
+        b2Body* body;
+
+        Entity entity;
+
+    private:
+        void _set_body(b2Body* b, Entity& e) {
+            this->body = b;
+            this->entity = e;
+#ifdef DEBUG
+            if (!this->body) {
+                // If we got a null b2Body...
+                throw std::invalid_argument("Expected a valid b2Body, got nullptr");
+            }
+
+            if (!e.isValid()) {
+                // If the given Entity isn't properly attached to a World
+                throw std::logic_error("Cannot use a PhysicsBodyComponent with an invalid Entity");
+            }
 #endif
 
-        this->body->SetUserData(&(this->entity));
-    }
-
-    /**
-     * Destructor. Tells the b2World to destroy @c this->body.
-     */
-    ~PhysicsBodyComponent() {
-        this->body->GetWorld()->DestroyBody(this->body);
-    }
-
-    /**
-     * Pointer to a @c b2Body that was created by the game's @c b2World. Not a
-     * smart pointer because Box2D objects are managed in a special way that a
-     * smart pointer would fuck up.
-     */
-    b2Body* body;
-
-    Entity entity;
+            this->body->SetUserData(&(this->entity));
+        }
 };
 
 }
 #endif // BOUNDINGVOLUMECOMPONENT_HPP
+
