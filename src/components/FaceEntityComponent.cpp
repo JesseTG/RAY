ddb748807b120622182035d7599ce9c580ab3743
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-03-14 19:06:13 -0400

    Updated the bindings for four of our own Component types

--- src/components/FaceEntityComponent.cpp
+++ src/components/FaceEntityComponent.cpp
@@ -1,10 +1,23 @@
 #include "FaceEntityComponent.hpp"
 
+#include <boost/optional.hpp>
+
 namespace ray {
+using boost::optional;
+
 FaceEntityComponent::FaceEntityComponent(const Entity& e) : target(e) {
 }
 
 void FaceEntityComponent::luaInit(LuaContext& lua) {
     lua.registerMember("target", &FaceEntityComponent::target);
+    lua.writeFunction("FaceEntityComponent", "new",
+    [](const optional<Entity&> e) {
+        if (e) {
+            return new FaceEntityComponent(*e);
+        }
+        else {
+            return new FaceEntityComponent;
+        }
+    });
 }
 }
