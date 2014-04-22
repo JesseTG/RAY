94a3f4b530abf2a5a5dd869baa8c60a54b426ace
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-04-22 17:16:31 -0400

    Made the EntityFollowSystem a bit more general
    -The Entity now jumps to its target if its speed is infinite

--- include/components/EntityFollowComponent.hpp
+++ include/components/EntityFollowComponent.hpp
@@ -3,6 +3,7 @@
 
 #include <anax/anax.hpp>
 #include <LuaContext.hpp>
+#include <cmath>
 
 namespace ray {
 
@@ -19,7 +20,7 @@ struct EntityFollowComponent : public anax::Component<EntityFollowComponent>
     /**
      * Constructs an EntityFollowComponent that is not following another @c Entity.
      */
-    EntityFollowComponent() {}
+    EntityFollowComponent() : maxSpeed(INFINITY) {}
 
     /**
      * Constructs an EntityFollowComponent that is following a given @c Entity.
@@ -27,13 +28,20 @@ struct EntityFollowComponent : public anax::Component<EntityFollowComponent>
      * @param e The @c Entity that @c *this's @c Entity will follow.
      * @c e.isValid() must be true, and @c e must have a PositionComponent.
      */
-    EntityFollowComponent(const Entity& e) : target(e) {}
+    EntityFollowComponent(const Entity& e) : target(e), maxSpeed(INFINITY) {}
+
+    EntityFollowComponent(const Entity& e, const float maxSpeed) : target(e), maxSpeed(maxSpeed) {}
 
     /**
      * The @c Entity that the @c Entity containing @c *this will follow. If
      * @c target.isValid() is @c false, @c *this has no effect on its @c Entity.
      */
     Entity target;
+
+    /**
+     * The maximum speed an Entity will follow another.  Defaults to zero
+     */
+    float maxSpeed;
 
     static void luaInit(LuaContext& lua);
 };
--- src/components/EntityFollowComponent.cpp
+++ src/components/EntityFollowComponent.cpp
@@ -5,9 +5,13 @@ using boost::optional;
 
 void EntityFollowComponent::luaInit(LuaContext& lua) {
     lua.registerMember("target", &EntityFollowComponent::target);
+    lua.registerMember("maxSpeed", &EntityFollowComponent::maxSpeed);
     lua.writeFunction("EntityFollowComponent", "new",
-    [](const optional<Entity&> e) {
-        if (e) {
+    [](const optional<Entity&> e, const optional<float> maxSpeed) {
+        if (e && maxSpeed) {
+            return new EntityFollowComponent(*e, *maxSpeed);
+        }
+        else if (e) {
             return new EntityFollowComponent(*e);
         }
         else {
