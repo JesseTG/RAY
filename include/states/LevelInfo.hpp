afb4f040c38ea66fa85c25dbba766c77eef9651d
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-04-30 12:44:26 -0400

    Added in lives to LevelInfo

--- include/states/LevelInfo.hpp
+++ include/states/LevelInfo.hpp
@@ -7,8 +7,10 @@ namespace ray {
 using std::string;
 struct LevelInfo : public util::GameStateArguments
 {
+    LevelInfo(const string& name="", const int lives=0) : GameStateArguments(), name(name), lives(lives) {}
     ~LevelInfo() {}
     string name;
+    int lives;
 };
 }
 
